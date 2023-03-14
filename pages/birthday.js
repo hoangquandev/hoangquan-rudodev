7f4c414ae7489234e0e5150453dbb49bb65280cf
Hoàng Quân <lehoangquanit@gmail.com>
2023-03-14 17:11:28 +0700

    #

--- pages/birthday.js
+++ pages/birthday.js
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import Layout from '../components/layouts/article'
 import bannerImg from '../public/images/birthday/Home.png'
-import invitationImg from '../public/images/birthday/thegang.webp'
+import invitationImg from '../public/images/birthday/sinhnhat.jpeg'
 
 
 const Works = () => (
@@ -13,20 +13,44 @@ const Works = () => (
         </div>
         <div style={{
             display: "flex",
-            height: "500px"
+            height: "500px",
+            gap: "18px"
         }} >
             <div style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", overflow: "hidden" }}>
                 <Image src={invitationImg} />
             </div>
-            <div style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <div style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", fontSize: "12px" }}>
                 <div>
                     <h2 style={{ textTransform: "uppercase", color: "#fe5600", fontSize: "30px", marginBottom: "2rem" }}>Show up!</h2>
                     <p>March 17 | 7.30 PM</p>
                     <br />
-                    <p>The Gangs Urban <br />
-                        212 Lý Thái Tổ, Phường 1,<br />
-                        Quận 3, Thành phố Hồ Chí Minh</p>
+                    <p><b style={{ fontSize: "18px" }}>Da Lua beer garden</b> <br />
+                        131 Hoa Hung, Ward 12,<br />
+                        Dist 10, HCMC</p>
                 </div>
+            </div>
+        </div>
+        <div style={{ textAlign: "center" }}>
+            <h2 style={{ fontSize: "30px", color: "#fe5600", textTransform: "uppercase" }}>Important Details</h2>
+            <div>
+                Remember to bring your lover to the party!
+            </div>
+            <div>
+                <p style={{ textAlign: 'left', fontSize: "14px", color: "#fe5000", fontStyle: "italic", marginTop: '2rem' }}>My wish list:</p>
+                <ul style={{ textAlign: 'left', padding: "1rem", fontSize: "12px", fontStyle: "italic" }}>
+                    <li>
+                        Mechanical Keyboard
+                    </li>
+                    <li>
+                        Clothes
+                    </li>
+                    <li>
+                        Monitor
+                    </li>
+                    <li>
+                        perfume
+                    </li>
+                </ul>
             </div>
         </div>
     </Layout>
